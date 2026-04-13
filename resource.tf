ae40926341b2ae6a5a2d9febb5ec10aaddc9c4be
GopalSabale <gopalsabale4@gmail.com>
2026-04-13 15:36:44 +0530

    adding count

--- resource.tf
+++ resource.tf
@@ -1,5 +1,6 @@
 module "my_ec2" {
-  source      = "git::https://github.com/GopalSabale/aws_tf_modules.git//modules/ec2?ref=v1.1.2"
+  source      = "git::https://github.com/GopalSabale/aws_tf_modules.git//modules/ec2?ref=v1.1.3"
   instance_type = var.instance_type
   ami = var.ami
+  count = var.instance_count
 }
