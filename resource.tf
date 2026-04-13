875d48f71159d39c759b5be4592aadcc17bcb937
GopalSabale <gopalsabale4@gmail.com>
2026-04-13 15:08:00 +0530

    push new version

--- resource.tf
+++ resource.tf
@@ -1,5 +1,5 @@
 module "my_ec2" {
-  source      = "git::https://github.com/GopalSabale/aws_tf_modules.git//modules/ec2?ref=v1.1.1"
+  source      = "git::https://github.com/GopalSabale/aws_tf_modules.git//modules/ec2?ref=v1.1.2"
   instance_type = var.instance_type
   ami = var.ami
 }
